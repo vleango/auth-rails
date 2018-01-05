609791cc384a9f240f426f5c79ac63b8f0a64149
Tha Leang <vleango@gmail.com>
2018-01-04 22:39:30 -0800

    load schema instead of doing migration for test script

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "*** Dropping existing DB ***"
+echo "*** Dropping the database ***"
 RAILS_ENV=test rake db:drop --trace
 echo "*** Creating the database ***"
 RAILS_ENV=test rake db:create --trace
-echo "*** Migrating the database ***"
-RAILS_ENV=test rake db:migrate --trace
+echo "*** Loading the DB Schema ***"
+RAILS_ENV=test rake db:schema:load --trace
 echo "*** Running RSpec ***"
 rspec
