2d8e1953737526d0cfdff01c7dc2dccadfbd317e
Tha Leang <vleango@gmail.com>
2018-01-25 08:18:07 -0800

    Use scope to isolate namespace on controllers and views

---
+++ app/controllers/auth_rails/application_controller.rb
@@ -0,0 +1,20 @@
+module AuthRails
+  class ApplicationController < ActionController::API
+    include ErrorHandler
+
+    private
+
+    def authenticate
+      token = request.headers['x-auth']
+      @user = User.find_by_token(token)
+      set_token_to_header(@user.user_tokens.last.token)
+    end
+
+    private
+
+    def set_token_to_header(token)
+      response.headers['x-auth'] = token
+    end
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
-  namespace :v1, defaults: { format: :json } do
-    namespace :auth do
-      post '/login' => 'authenticate#login'
-      post '/register' => 'authenticate#register'
-      post '/provider' => 'authenticate#provider'
-      delete '/logout' => 'authenticate#logout'
+  scope module: 'auth_rails' do
+    namespace :v1, defaults: { format: :json } do
+      namespace :auth do
+        post '/login' => 'authenticate#login'
+        post '/register' => 'authenticate#register'
+        post '/provider' => 'authenticate#provider'
+        delete '/logout' => 'authenticate#logout'
+      end
     end
   end
 
